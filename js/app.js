be537ff3793c0a7ba331ceed01ecf5daaa8c4c04
mihin <huguesaime.mihin@gmail.com>
2020-12-27 12:49:26 +0000

    simplon

--- js/app.js
+++ js/app.js
@@ -24,3 +24,17 @@ const options = {
       observer.observe(r)
   })
   
+
+
+
+  $('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 2000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
